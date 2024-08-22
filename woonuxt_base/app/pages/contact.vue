1c02aa35fd0a75ec40e7a93b23fd640867553970
Zubair Tofy <tofy.zubair@gmail.com>
2024-08-22 17:12:44 +0400

    Contact Page

--- woonuxt_base/app/pages/contact.vue
+++ woonuxt_base/app/pages/contact.vue
@@ -1,19 +1,47 @@
 <template>
   <div class="container my-8">
-    <h1 class="mb-8 text-3xl font-semibold text-primary">Contact Page</h1>
+    <h1 class="mb-8 text-3xl font-semibold text-primary">About Us</h1>
     <div class="my-8 prose">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat facilis, commodi illum repellat accusamus praesentium quaerat ut ducimus ipsam deserunt, a voluptate id
-        similique ullam, provident tenetur molestiae ad. Ea aperiam esse eaque aut, beatae consectetur nam velit unde! Et excepturi quam eos labore, nulla impedit recusandae
-        quaerat maxime expedita.
+        What makes us special is our uniqueness, and what makes us better is how dedicated we are in achieving it.
+      </p>
+      <h2>Who is Nairaa?</h2>
+      <p>
+        Nairaa, a blend of my husband's name and mine, has always been an immense source of joy and inspiration for me. Created after the birth of my little bundle of joy, Nairaa's Art and Crafts is a small business specializing in arts and crafts. We focus on personalized gift items, including resin art showcasing various products and painting & calligraphy on canvas.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo voluptate neque in odio officia assumenda nulla cumque. Veniam et tenetur accusantium, fuga pariatur
-        repellendus impedit. Quod sint odio quisquam! Sequi repellendus eum dolorum eveniet, quidem dolor necessitatibus velit sunt esse maiores. Eum odit dolorum mollitia?
-        Adipisci amet hic veniam debitis architecto nobis necessitatibus inventore iure ullam cum illo quae quam, excepturi beatae itaque exercitationem magnam quidem aspernatur
-        iusto. Quos, reprehenderit? Dignissimos autem recusandae qui sit cum velit consequatur pariatur illum omnis excepturi, adipisci ab eius, optio, temporibus iure unde
-        inventore. Maxime officiis tempore voluptatum ipsa, ab possimus atque corporis repellendus obcaecati, numquam fuga ullam doloremque ea reprehenderit earum quam culpa neque
-        dolor sint voluptates qui rem? Ipsam reiciendis non illum?
+        Since the foundation of Nairaa's Art and Crafts on 27th July 2020, we have brought huge smiles to our dear clients through our personalized products. Being someone who loves art and crafts, I always enjoy launching new products and experimenting with new models. Despite leading a very hectic life along with being a wife and a mother, I always try my best to meet the expectations of my clients. Sometimes, I have to spend sleepless nights just to complete my orders.
+      </p>
+      <p>
+        Managing a small business is not only a long-cherished dream; it also allows me to find solace in art. Through this business venture, all I want is more exposure to a greater audience and to provide quality art/products to all my customers.
+      </p>
+
+      <h2>Book Your Date</h2>
+      <p>
+        Designing and creating amazing seasonal products is our passion. Book and secure your date! This website allows you to secure a date if you are looking to shop with us. Please allow us a minimum time frame of 3 weeks for regular custom-made orders and a turnaround of 8 to 12 weeks for keepsakes and preservation.
+      </p>
+      <blockquote>
+        "Creativity is a natural extension of our enthusiasm." - Earl Nightingale
+      </blockquote>
+      <p>
+        You can always reach out to us directly through WhatsApp. We will try to respond as quickly as possible! However, during peak seasons like Eid or other festive occasions, we may be swamped with orders, and it might take up to 48 hours to respond to queries.
+      </p>
+      <p>
+        We put forth a lot of effort, love, and dedication for the things we believe in and the people we care about. If the workload becomes more than intended, you will be notified when orders are closed and reopened!
+      </p>
+
+      <h2>Why Choose Nairaa's...</h2>
+      <h3>Professional</h3>
+      <p>
+        When interacting with us, you'll notice that we try to maintain our transactions as professional as possible. We believe that by doing this, our clients will be able to relax knowing that we take their orders seriously and value their confidence! Visit our social media pages to have a better understanding of our exquisite artwork.
+      </p>
+      <h3>Inspiration</h3>
+      <p>
+        Big thanks to all our passionate and loyal customers, family, and friends who continue to inspire us and keep us focused. Being an aesthete, we really enjoy crafting your artwork, experimenting, and launching new products. Best of all, with our customized products, we've been able to make our loyal customers grin a lot.
+      </p>
+      <h3>Goals</h3>
+      <p>
+        Receiving tons of recommendations and encouragement from both our supporters and clients after lots of hard work and dedication. We've been granted opportunities to be part of Phoenix Mall's Eid Market Fair twice, allowing us to have personal interactions with our regular clients or even work on big corporate orders of more than 500 resin pieces.
       </p>
     </div>
   </div>
